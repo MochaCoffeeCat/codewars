268fb1e64d7ae1959b6879a4f88dffcfb732cbbb
Mocha <15025153960@163.com>
2018-01-04 10:17:32 +0800

    XO

---
+++ January/src/com/mocha/SumOfOddNumbers/RowSumOddNumbers.java
@@ -0,0 +1,11 @@
+package com.mocha.SumOfOddNumbers;
+
+public class RowSumOddNumbers {
+    private static int rowSumOddNumbers(int n) {
+        return (int) ((Math.pow(((Math.pow(n, 2) + n) / 2), 2)) - (Math.pow(((Math.pow((n - 1), 2) + (n - 1)) / 2), 2)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RowSumOddNumbers.rowSumOddNumbers(42));
+    }
+}
